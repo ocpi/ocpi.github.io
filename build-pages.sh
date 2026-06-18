d72a311204c64c043a9564b7b55c4796c9c980d6
Reinier Lamers <reinier.lamers@swtchenergy.com>
2026-06-18 15:27:29 -0600

    fix directory changing logic

--- build-pages.sh
+++ build-pages.sh
@@ -5,8 +5,9 @@ set -e
 mkdir pages-out
 
 for proposal_dir in proposals/* ; do
-  cd proposal_dir
+  cd "$proposal_dir"
   asciidoctor *.asciidoc
   proposal_title=`grep '^= ' *.asciidoc`
   echo proposal_title >> pages-out/index.html
+  cd ../..
 done
