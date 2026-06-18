26d9eb558d8a0d083eb6942325b6f9f55855a6dc
Reinier Lamers <reinier.lamers@swtchenergy.com>
2026-06-18 15:23:39 -0600

    build pages instead of just copying index.html

---
+++ build-pages.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+mkdir pages-out
+
+for proposal_dir in proposals/* ; do
+  cd proposal_dir
+  asciidoctor *.asciidoc
+  proposal_title=`grep '^= ' *.asciidoc`
+  echo proposal_title >> pages-out/index.html
+done
